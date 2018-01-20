c5c7763e5aa7e45bd4c6cdf7ad220ad254df025a
BuildTools <unconfigured@null.spigotmc.org>
2018-01-20 14:23:41 +0200

    Initial

---
+++ Geopeitus_Logi/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WB_MainAction = New System.Windows.Forms.WebBrowser()
+        Me.Btn_Fire = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LogBox = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'WB_MainAction
+        '
+        Me.WB_MainAction.Location = New System.Drawing.Point(555, 83)
+        Me.WB_MainAction.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WB_MainAction.Name = "WB_MainAction"
+        Me.WB_MainAction.ScriptErrorsSuppressed = True
+        Me.WB_MainAction.Size = New System.Drawing.Size(377, 506)
+        Me.WB_MainAction.TabIndex = 1
+        '
+        'Btn_Fire
+        '
+        Me.Btn_Fire.Location = New System.Drawing.Point(682, 12)
+        Me.Btn_Fire.Name = "Btn_Fire"
+        Me.Btn_Fire.Size = New System.Drawing.Size(250, 65)
+        Me.Btn_Fire.TabIndex = 2
+        Me.Btn_Fire.Text = "Anna tuld!"
+        Me.Btn_Fire.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(426, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(250, 65)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Test"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LogBox
+        '
+        Me.LogBox.HideSelection = False
+        Me.LogBox.Location = New System.Drawing.Point(12, 83)
+        Me.LogBox.Name = "LogBox"
+        Me.LogBox.ReadOnly = True
+        Me.LogBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical
+        Me.LogBox.Size = New System.Drawing.Size(537, 506)
+        Me.LogBox.TabIndex = 4
+        Me.LogBox.Text = ""
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(944, 601)
+        Me.Controls.Add(Me.LogBox)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Btn_Fire)
+        Me.Controls.Add(Me.WB_MainAction)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Main"
+        Me.Text = "Geopeituses 1000 aarde logimine võib osutuda üsna tüütuks"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WB_MainAction As WebBrowser
+    Friend WithEvents Btn_Fire As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents LogBox As RichTextBox
+End Class
---
+++ Geopeitus_Logi/Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Main
+
+    Private Property pageready As Boolean = False
+
+    Public hanneseStat As String = "http://www.geopeitus.ee/?p=420&u=hpalang1&num=10000"
+	Public kauriStat As String = "http://www.geopeitus.ee/index.php?p=420&u=kaurp&num=10000"
+	Public geoMain As String = "http://www.geopeitus.ee"
+    Public logBase As String = "http://www.geopeitus.ee/logs/new/c/"
+    Private username As String = "kaurp"
+    Private password As String = "cog6heat"
+
+	Dim caches As New Dictionary(Of String, List(Of String))
+
+	Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		'WB_HanneseStat.Navigate(hanneseStat)
+		'WaitForPageLoadStat()
+		'logIn(WB_HanneseStat)
+	End Sub
+	Private Sub Btn_Fire_Click(sender As Object, e As EventArgs) Handles Btn_Fire.Click
+		WB_MainAction.Navigate(geoMain)
+		WaitForPageLoad()
+
+		logIn()
+		WaitForPageLoad()
+
+		WB_MainAction.Navigate(hanneseStat)
+		WaitForPageLoad()
+
+		generateToLog()
+
+		WB_MainAction.Navigate(kauriStat)
+		WaitForPageLoad()
+
+		cleanToLog()
+
+		Dim pair As KeyValuePair(Of String, List(Of String))
+		For Each pair In caches
+			logEntry(pair.Value(0), pair.Key, pair.Value(3))
+		Next
+
+		LogBox.AppendText("-------------------------------------------------------------------------------------------------------------------" & Environment.NewLine)
+		LogBox.AppendText("Fucking everything should be logged" & Environment.NewLine)
+	End Sub
+
+	Private Sub logIn()
+		Dim allInputs As HtmlElementCollection = WB_MainAction.Document.GetElementsByTagName("input")
+
+		For Each input As HtmlElement In allInputs
+			Dim name As String = input.GetAttribute("name")
+
+			If name = "LoginForm[username]" Then
+				input.InnerText = username
+			End If
+			If name = "LoginForm[password]" Then
+				input.InnerText = password
+			End If
+			If name = "submit" Then
+				input.InvokeMember("click")
+			End If
+		Next
+	End Sub
+	Private Sub generateToLog()
+		Dim tables As HtmlElementCollection = WB_MainAction.Document.GetElementsByTagName("table")
+
+		Debug.WriteLine("Number of tables: " & tables.Count)
+		Dim table As HtmlElement = tables.Item(0)
+		Dim logs As HtmlElementCollection = table.GetElementsByTagName("tr")
+		For x As Integer = 0 To logs.Count - 1
+			Dim log As HtmlElementCollection = logs.Item(x).Children
+
+			If log.Item(3).InnerText.ToLower.Contains("leidis") Then
+				Dim group As String = log.Item(4).InnerText.ToLower
+				If group.Contains("kaur") Or group.Contains("kogu jõuk") Then
+					Dim foundDate As String = log.Item(1).InnerText
+					Dim id As String = extractID(log.Item(2).Children.Item(0).GetAttribute("href"))
+					Dim name As String = log.Item(2).InnerText
+					addToLogList(correctDate(foundDate), id, name, group)
+					LogBox.AppendText("Added: " & name & tab(1) & " ID: " & id & Environment.NewLine)
+				End If
+			End If
+		Next
+		LogBox.AppendText("-------------------------------------------------------------------------------------------------------------------" & Environment.NewLine)
+	End Sub
+
+	Private Sub cleanToLog()
+		Dim tables As HtmlElementCollection = WB_MainAction.Document.GetElementsByTagName("table")
+
+		Debug.WriteLine("Number of tables: " & tables.Count)
+		Dim table As HtmlElement = tables.Item(0)
+		Dim logs As HtmlElementCollection = table.GetElementsByTagName("tr")
+		For x As Integer = 0 To logs.Count - 1
+			Dim log As HtmlElementCollection = logs.Item(x).Children
+
+			If log.Item(3).InnerText.ToLower.Contains("leidis") Then
+				Dim id As String = extractID(log.Item(2).Children.Item(0).GetAttribute("href"))
+				If (caches.Contains(extractID(id))) Then
+					caches.Remove(extractID(id))
+				End If
+			End If
+		Next
+		LogBox.AppendText("-------------------------------------------------------------------------------------------------------------------" & Environment.NewLine)
+	End Sub
+
+	Private Sub logEntry(foundDate As String, id As String, group As String)
+		Delay(3)
+		WB_MainAction.Navigate(logBase & id)
+		WaitForPageLoad()
+
+		Dim found As HtmlElement = WB_MainAction.Document.GetElementsByTagName("option").Cast(Of HtmlElement).First(
+			Function(el) el.GetAttribute("value") = "1")
+
+		found.SetAttribute("selected", "true")
+
+		WB_MainAction.Document.GetElementById("Event_date").InnerText = foundDate
+		WB_MainAction.Document.GetElementById("Event_user_name").InnerText = group
+		WB_MainAction.Document.GetElementById("wmd-input").InnerText = "Leitud"
+
+		For Each input As HtmlElement In WB_MainAction.Document.GetElementsByTagName("input")
+			Dim name As String = input.GetAttribute("name")
+
+			If name = "yt0" Then
+				input.InvokeMember("click")
+				LogBox.AppendText("Logging cache with ID: " & id & Environment.NewLine)
+				WaitForPageLoad()
+			End If
+		Next
+	End Sub
+#Region "Page Loading Functions"
+	Private Sub WaitForPageLoad()
+		AddHandler WB_MainAction.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf PageWaiter)
+		While Not pageready
+			Application.DoEvents()
+		End While
+		pageready = False
+	End Sub
+	Private Sub PageWaiter(ByVal sender As Object, ByVal e As WebBrowserDocumentCompletedEventArgs)
+		If WB_MainAction.ReadyState = WebBrowserReadyState.Complete Then
+			pageready = True
+			RemoveHandler WB_MainAction.DocumentCompleted, New WebBrowserDocumentCompletedEventHandler(AddressOf PageWaiter)
+		End If
+	End Sub
+#End Region
+	Private Sub wbMain_Navigating(ByVal sender As Object, ByVal e As WebBrowserNavigatingEventArgs) Handles WB_MainAction.Navigating
+		Debug.WriteLine("Main navigating")
+	End Sub
+	Private Sub addToLogList(foundDate As String, id As String, name As String, group As String)
+		caches.Add(id, New List(Of String)(New String() {correctDate(foundDate), name, group}))
+		LogBox.AppendText("Cache: " & name & tab(2) & " Found: " & correctDate(foundDate) & tab(2) & " Group: " & group & Environment.NewLine)
+	End Sub
+	Private Function correctDate(foundDate As String)
+		Dim returnDate() As String = foundDate.Split(".")
+		Return returnDate(2) & "-" & returnDate(1) & "-" & returnDate(0)
+	End Function
+	Private Function extractID(id As String)
+		Dim sDelimStart As String = "&c=" 'First delimiting word
+		Dim sDelimEnd As String = "#" 'Second delimiting word
+		Dim nIndexStart As Integer = id.IndexOf(sDelimStart) 'Find the first occurrence of f1
+		Dim nIndexEnd As Integer = id.IndexOf(sDelimEnd) 'Find the first occurrence of f2
+
+		If nIndexStart > -1 AndAlso nIndexEnd > -1 Then '-1 means the word was not found.
+			Dim res As String = Strings.Mid(id, nIndexStart + sDelimStart.Length + 1, nIndexEnd - nIndexStart - sDelimStart.Length) 'Crop the text between
+			Return res
+		End If
+		Return "empty"
+	End Function
+	Private Function tab(count As Integer)
+        Dim tabs As String = String.Empty
+        For x As Integer = 1 To count
+            tabs = tabs & ControlChars.Tab
+        Next
+        Return tabs
+    End Function
+End Class
